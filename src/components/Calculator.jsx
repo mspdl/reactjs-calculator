7ea2576369899f30d2f95158f902000be2b11e27
Morgan <morgan.espindola@gmail.com>
2023-12-15 18:14:01 -0300

    Project Implementation

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,12 @@
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="complete-operation"> 3 + 3 = 6</div>
+      <div className="display">6</div>
+      <div className="buttons"></div>
+      <button>=</button>
+    </div>
+  );
+}
+
+export default Calculator;
