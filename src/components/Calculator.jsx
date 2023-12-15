ab4e2660afc8350e5314eca6dc38870fb51858ec
Morgan <morgan.espindola@gmail.com>
2023-12-15 18:24:05 -0300

    Added styles

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,23 @@
+import "./Calculator.css";
+
 function Calculator() {
+  const keypadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const operations = ["+", "-", "*", "/"];
+
   return (
     <div className="calculator">
-      <div className="complete-operation"> 3 + 3 = 6</div>
-      <div className="display">6</div>
-      <div className="buttons"></div>
+      <div className="complete-operation"></div>
+      <div className="display"></div>
+      <div className="buttons">
+        <button>AC</button>
+        {keypadNumbers.map((number) => (
+          <button key={number}>{number}</button>
+        ))}
+        {operations.map((operation) => (
+          <button key={operation}>{operation}</button>
+        ))}
       <button>=</button>
+      </div>
     </div>
   );
 }
